4a6b1f44c3f109e05488b8511c3234a1e5fe58a5
Mpus <mpodmokly@student.agh.edu.pl>
2023-11-18 18:44:38 +0100

    lab2

--- jarvis_algorithm.py
+++ jarvis_algorithm.py
@@ -1,8 +1,12 @@
 from copy import copy
+from math import sqrt
 
 def mat_det_2x2(a, b, c):
     det = (a[0] - c[0]) * (b[1] - c[1]) - (b[0] - c[0]) * (a[1] - c[1])
     return det
+
+def distance(p1, p2):
+    return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 def jarvis_algorithm(Q):
     n = len(Q)
@@ -29,14 +33,12 @@ def jarvis_algorithm(Q):
 
     while True:
         for i in range(n):
-            if mat_det_2x2(S[a], S[b], S[i]) < 0:
+            det = mat_det_2x2(S[a], S[b], S[i])
+            if det < 0 or (det == 0 and distance(S[a], S[i]) > distance(S[a], S[b])):
                 b = i
         
         if b == left_idx:
             break
-        
-        if len(points) > 1 and mat_det_2x2(points[-2], points[-1], S[b]) == 0:
-            points.pop()
         
         points.append(S[b])
         a = b
